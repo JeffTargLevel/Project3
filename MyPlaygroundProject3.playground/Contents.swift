64e09075692dfa0e479489a302a944a7c218125c
JeffGitHub <jeff.santolaria05@gmail.com>
2018-03-02 11:23:20 +0100

    Create class Weapon and subclasses

--- MyPlaygroundProject3.playground/Contents.swift
+++ MyPlaygroundProject3.playground/Contents.swift
@@ -4,8 +4,8 @@ import UIKit
 
 // Create class character
 
-class Character {
-    var name: String
+ class Character {
+    var name: String = ""
     var description = ""
     var lifePoints = 0
     
@@ -110,5 +110,57 @@ class Dwarf: Character {
     
 }
 
-var gandalf = Magus.init(name: "gandalf")
-gandalf.lifePoints
+// Create class weapon
+
+class Weapon {
+    var damages = 0
+    var treat = 0
+    
+    
+}
+
+class Sword: Weapon {
+    override var damages: Int {
+        get {
+            return super.damages + 10
+        }
+        set {
+            super.damages = newValue
+        }
+    }
+}
+
+class MageBaton: Weapon {
+    override var treat: Int {
+        get {
+            return super.treat + 10
+        }
+        set {
+           super.treat = newValue
+        }
+    }
+    
+}
+
+class Fists: Weapon {
+    override var damages: Int {
+        get {
+            return super.damages + 5
+        }
+        set {
+            super.damages = newValue
+        }
+    }
+    
+}
+
+class Axe: Weapon {
+    override var damages: Int {
+        get {
+            return super.damages + 20
+        }
+        set {
+            super.damages = newValue
+        }
+    }
+}
